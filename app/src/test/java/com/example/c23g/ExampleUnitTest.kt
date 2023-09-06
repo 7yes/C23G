0d52e035d49d385d9d2f4bd94dc8285648dd19dd
7yes <100316346+7yes@users.noreply.github.com>
2023-09-06 08:43:42 -0500

    base

--- app/src/test/java/com/example/c23g/ExampleUnitTest.kt
+++ app/src/test/java/com/example/c23g/ExampleUnitTest.kt
@@ -2,8 +2,6 @@ package com.example.c23g
 
 import org.junit.Test
 
-import org.junit.Assert.*
-
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -12,6 +10,6 @@ import org.junit.Assert.*
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+ //       assertEquals(4, 2 + 2)
     }
 }
