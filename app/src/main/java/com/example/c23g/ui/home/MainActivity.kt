7b67b3dd4c6eb37c99a1302a1868d1ac441488db
7yes <100316346+7yes@users.noreply.github.com>
2023-09-15 17:15:58 -0500

    navigation

--- app/src/main/java/com/example/c23g/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/c23g/ui/home/MainActivity.kt
@@ -2,16 +2,29 @@ package com.example.c23g.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.c23g.R
 import com.example.c23g.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var navControler: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        initNavigation()
+
         setContentView(binding.root)
+    }
+    private fun initNavigation() {
+        val navHost: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.frag_container) as NavHostFragment
+        val navController = navHost.navController
+        binding.bottomNavView.setupWithNavController(navController)
     }
 }
