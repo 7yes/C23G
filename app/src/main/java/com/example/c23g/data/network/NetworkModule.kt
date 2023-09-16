fc78dbd97c8d6543a60e882141462669eef6b92b
7yes <100316346+7yes@users.noreply.github.com>
2023-09-16 18:47:11 -0500

    interceptor

--- app/src/main/java/com/example/c23g/data/network/NetworkModule.kt
+++ app/src/main/java/com/example/c23g/data/network/NetworkModule.kt
@@ -1,11 +1,14 @@
 package com.example.c23g.data.network
 
 import com.example.c23g.data.RepositoryImpl
+import com.example.c23g.data.network.core.interceptors.AuthInterceptor
 import com.example.c23g.domain.Repository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -15,10 +18,22 @@ import javax.inject.Singleton
 object NetworkModule {
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://newastro.vercel.app")
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(authInterceptor: AuthInterceptor): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(interceptor)
+            .addInterceptor(authInterceptor)
             .build()
     }
 
@@ -28,8 +43,7 @@ object NetworkModule {
     }
 
     @Provides
-    fun providesRepository(apiService:ApiService):Repository{
+    fun providesRepository(apiService: ApiService): Repository {
         return RepositoryImpl(apiService)
     }
-
 }
