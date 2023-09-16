a7404bd19ec205eb72ca6b6158486902226649f2
7yes <100316346+7yes@users.noreply.github.com>
2023-09-16 07:28:23 -0500

    supuestamente hecha aimacion pero no jala

--- app/src/main/java/com/example/c23g/ui/horoscope/adapter/HoroscopeViewHolder.kt
+++ app/src/main/java/com/example/c23g/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.c23g.ui.horoscope.adapter
 
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.example.c23g.databinding.ItemHoroscoppBinding
 import com.example.c23g.domain.model.HoroscopeInfo
@@ -11,6 +12,16 @@ class HoroscopeViewHolder(view:View):RecyclerView.ViewHolder(view) {
         val context = binding.tvTitle.context
         binding.ivHoroscope.setImageResource(item.img)
         binding.tvTitle.text = context.getString(item.name)
-        itemView.setOnClickListener { onClickLis(item) }
+       // itemView.setOnClickListener { onClickLis(item) }
+        startRotationAnimation(binding.ivHoroscope, newLambda = {onClickLis(item)} )
+    }
+}
+private fun startRotationAnimation(view:View, newLambda:()->Unit){
+    view.animate().apply {
+        duration = 500
+        interpolator = LinearInterpolator()
+        rotationBy(360f)
+        withEndAction { newLambda() }
+        start()
     }
 }
